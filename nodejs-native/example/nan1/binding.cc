94fb8e179f42b0bea99094e60b097f1a3a3d9b06
Prabhakar Patil <PrabhakarRPatil@eaton.com>
2021-03-07 21:29:23 +0530

    adding C++ addons using nodejs NaN w/ macros

---
+++ nodejs-native/example/nan1/binding.cc
@@ -0,0 +1,40 @@
+#include <nan.h>
+
+const int maxValue = 10;
+int numberOfCalls = 0;
+
+void WhoAmI(const Nan::FunctionCallbackInfo<v8::Value>& args) {
+  auto message = Nan::New<v8::String>("I'm a Node Hero!").ToLocalChecked();
+  args.GetReturnValue().Set(message);
+}
+
+void Increment(const Nan::FunctionCallbackInfo<v8::Value>& args) {
+
+  if (!args[0]->IsNumber()) {
+    Nan::ThrowError("Argument must be a number");
+    return;
+  }
+
+  // double argsValue = 4;
+  double argsValue = args[0]->NumberValue();
+  if (numberOfCalls + argsValue > maxValue) {
+    Nan::ThrowError("Counter went through the roof!");
+    return;
+  }
+
+  numberOfCalls += argsValue;
+
+  auto currentNumberOfCalls = Nan::New<v8::Number>(numberOfCalls);
+
+  args.GetReturnValue().Set(currentNumberOfCalls);
+}
+
+void Initialize(v8::Local<v8::Object> exports) {
+  exports->Set(Nan::New("whoami").ToLocalChecked(),
+      Nan::New<v8::FunctionTemplate>(WhoAmI)->GetFunction());
+
+  exports->Set(Nan::New("increment").ToLocalChecked(),
+      Nan::New<v8::FunctionTemplate>(Increment)->GetFunction());
+}
+
+NODE_MODULE(addon, Initialize)
